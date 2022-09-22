342d96fa7c556fa280651741260789188553fa4f
Emmanuel Adjei <paayima94@gmail.comgit config --global user.email paayima94@gmail.com>
2022-09-22 00:05:18 +0000

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # creates a file in the /tmp directory
-file
-{ '/tmp/school':
+file { '/tmp/holberton':
   ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
 }
-
+\n
